8333254064e312162166f54061f7bb9585f7fab8
Patrigan <patrick.provinciael@gmail.com>
2021-01-28 17:00:53 +0100

    Update Slime Lab chest loot

--- src/main/java/mod/patrigan/slimierslimes/datagen/ModChestLootTables.java
+++ src/main/java/mod/patrigan/slimierslimes/datagen/ModChestLootTables.java
@@ -10,11 +10,18 @@ import net.minecraft.util.ResourceLocation;
 import java.util.function.BiConsumer;
 
 import static mod.patrigan.slimierslimes.SlimierSlimes.MOD_ID;
+import static net.minecraftforge.common.Tags.Items.SLIMEBALLS;
 
 public class ModChestLootTables extends ChestLootTables {
 
     @Override
     public void accept(BiConsumer<ResourceLocation, LootTable.Builder> consumer) {
-        consumer.accept(new ResourceLocation(MOD_ID, "chests/pillager_slime_lab"), LootTable.builder().addLootPool(LootPool.builder().rolls(RandomValueRange.of(6.0F, 10.0F)).addEntry(TagLootEntry.getBuilder(ModTags.Items.JELLY).weight(8).acceptFunction(SetCount.builder(RandomValueRange.of(1.0F, 4.0F)))).addEntry(ItemLootEntry.builder(Items.BREAD).weight(5).acceptFunction(SetCount.builder(RandomValueRange.of(1.0F, 4.0F))))));
+        consumer.accept(new ResourceLocation(MOD_ID, "chests/pillager_slime_lab"),
+                LootTable.builder().
+                        addLootPool(LootPool.builder().rolls(RandomValueRange.of(5.0F, 8.0F))
+                                .addEntry(TagLootEntry.getBuilder(ModTags.Items.JELLY).weight(6).acceptFunction(SetCount.builder(RandomValueRange.of(2.0F, 5.0F))))
+                                .addEntry(TagLootEntry.getBuilder(SLIMEBALLS).weight(3).acceptFunction(SetCount.builder(RandomValueRange.of(1.0F, 5.0F))))
+                                .addEntry(ItemLootEntry.builder(Items.BREAD).weight(6).acceptFunction(SetCount.builder(RandomValueRange.of(1.0F, 4.0F))))
+                                .addEntry(ItemLootEntry.builder(Items.COAL).weight(3).acceptFunction(SetCount.builder(RandomValueRange.of(1.0F, 3.0F))))));
     }
 }

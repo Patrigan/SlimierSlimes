5fa1f19fb3447ea45d379480b31cbf78141dea67
Patrigan <patrick.provinciael@gmail.com>
2021-05-23 12:21:52 +0200

    Introduction of Structure Toolkit

--- src/main/java/mod/patrigan/slimierslimes/init/ModProcessors.java
+++ src/main/java/mod/patrigan/slimierslimes/init/ModProcessors.java
@@ -10,35 +10,13 @@ import net.minecraft.world.gen.feature.template.StructureProcessor;
 import static mod.patrigan.slimierslimes.SlimierSlimes.MOD_ID;
 
 public class ModProcessors {
-    public static IStructureProcessorType<BlockMossifyProcessor> BLOCK_MOSSIFY;
     public static IStructureProcessorType<BlockSlimifyProcessor> BLOCK_SLIMIFY;
-    public static IStructureProcessorType<AirRetainerProcessor> AIR_RETAINER;
-    public static IStructureProcessorType<GradientSpotReplaceProcessor> GRADIENT_SPOT_REPLACE;
-    public static IStructureProcessorType<CeilingAttachmentProcessor> CEILING_ATTACHMENT;
-    public static IStructureProcessorType<VineProcessor> VINES;
-    public static IStructureProcessorType<MushroomProcessor> MUSHROOMS;;
-    public static IStructureProcessorType<FlowerPotProcessor> FLOWER_POT;
-    public static IStructureProcessorType<LilyPadProcessor> LILY_PADS;
     public static IStructureProcessorType<GooProcessor> GOO_BLOCKS;
-    public static IStructureProcessorType<SpawnerRandomizerProcessor> SPAWNER_RANDOMIZER_PROCESSOR;
-    public static IStructureProcessorType<WaterloggingFixProcessor> WATERLOGGING_FIX_PROCESSOR;
-    public static IStructureProcessorType<WaterflowFixProcessor> WATERFLOW_FIX_PROCESSOR;
 
 
     public static void init(){
-        BLOCK_MOSSIFY = register("block_mossify", BlockMossifyProcessor.CODEC);
         BLOCK_SLIMIFY = register("block_slimify", BlockSlimifyProcessor.CODEC);
-        AIR_RETAINER = register("air_retainer", AirRetainerProcessor.CODEC);
-        GRADIENT_SPOT_REPLACE = register("gradient_spot_replace", GradientSpotReplaceProcessor.CODEC);
-        CEILING_ATTACHMENT = register("ceiling_attachment", CeilingAttachmentProcessor.CODEC);
-        VINES = register("vines", VineProcessor.CODEC);
-        MUSHROOMS = register("mushrooms", MushroomProcessor.CODEC);
-        FLOWER_POT = register("flower_pot", FlowerPotProcessor.CODEC);
-        LILY_PADS = register("lily_pads", LilyPadProcessor.CODEC);
         GOO_BLOCKS = register("goo_blocks", GooProcessor.CODEC);
-        SPAWNER_RANDOMIZER_PROCESSOR = register("spawner_randomizer", SpawnerRandomizerProcessor.CODEC);
-        WATERLOGGING_FIX_PROCESSOR = register("waterlogging_fix", WaterloggingFixProcessor.CODEC);
-        WATERFLOW_FIX_PROCESSOR = register("waterflow_fix", WaterflowFixProcessor.CODEC);
     }
 
     static <P extends StructureProcessor> IStructureProcessorType<P> register(String name, Codec<P> codec) {

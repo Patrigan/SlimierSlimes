e5c58fe16f052b87eb3d4c8899e676611741185b
Patrigan <patrick.provinciael@gmail.com>
2021-06-13 23:51:22 +0200

    Working Lectern Processor

--- src/main/java/mod/patrigan/slimierslimes/datagen/ModChestLootTables.java
+++ src/main/java/mod/patrigan/slimierslimes/datagen/ModChestLootTables.java
@@ -5,7 +5,15 @@ import net.minecraft.data.loot.ChestLootTables;
 import net.minecraft.item.Items;
 import net.minecraft.loot.*;
 import net.minecraft.loot.functions.SetCount;
+import net.minecraft.loot.functions.SetNBT;
+import net.minecraft.nbt.CompoundNBT;
+import net.minecraft.nbt.INBT;
+import net.minecraft.nbt.ListNBT;
+import net.minecraft.nbt.StringNBT;
+import net.minecraft.tags.ItemTags;
 import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.text.ITextComponent;
+import net.minecraftforge.common.Tags;
 
 import java.util.function.BiConsumer;
 
@@ -23,5 +31,24 @@ public class ModChestLootTables extends ChestLootTables {
                                 .add(TagLootEntry.expandTag(SLIMEBALLS).setWeight(3).apply(SetCount.setCount(RandomValueRange.between(1.0F, 5.0F))))
                                 .add(ItemLootEntry.lootTableItem(Items.BREAD).setWeight(6).apply(SetCount.setCount(RandomValueRange.between(1.0F, 4.0F))))
                                 .add(ItemLootEntry.lootTableItem(Items.COAL).setWeight(3).apply(SetCount.setCount(RandomValueRange.between(1.0F, 3.0F))))));
+        consumer.accept(new ResourceLocation(MOD_ID, "processors/saplings"),
+                LootTable.lootTable().
+                        withPool(LootPool.lootPool().setRolls(ConstantRange.exactly(1))
+                                .add(TagLootEntry.expandTag(ItemTags.SAPLINGS).setWeight(1))));
+        createBooksLootTable(consumer);
+
+    }
+
+    private void createBooksLootTable(BiConsumer<ResourceLocation, LootTable.Builder> consumer) {
+        CompoundNBT compoundNBT = new CompoundNBT();
+        compoundNBT.putString("author", "Patrigan");
+        compoundNBT.putString("title", "TestSuccess");
+        ListNBT listNBT = new ListNBT();
+        listNBT.add(StringNBT.valueOf("This is a success"));
+        compoundNBT.put("pages", listNBT);
+        consumer.accept(new ResourceLocation(MOD_ID, "processors/books_test"),
+                LootTable.lootTable().
+                        withPool(LootPool.lootPool().setRolls(ConstantRange.exactly(1))
+                                .add(ItemLootEntry.lootTableItem(Items.WRITTEN_BOOK).setWeight(1).apply(SetNBT.setTag(compoundNBT)))));
     }
 }
